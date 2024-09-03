2616ae2bd77126c7a5427f788e3c0a71cf1eb242
Suraj Godse <godsesuraj2140@gmail.com>
2024-09-03 01:42:01 -0500

    Update Firebase.js

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -3,13 +3,13 @@ import { getDatabase, ref, get } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD-v5gNG_aLP4o5uqGET03yHG1izhG-A9o",
+  apiKey: "******************",
   authDomain: "expensetrack-5bc40.firebaseapp.com",
-  databaseURL: "https://expensetrack-5bc40-default-rtdb.firebaseio.com",
-  projectId: "expensetrack-5bc40",
-  storageBucket: "expensetrack-5bc40.appspot.com",
-  messagingSenderId: "778250175101",
-  appId: "1:778250175101:web:5bab44dbdf95bb73b6e714"
+  databaseURL: "******************",
+  projectId: "********************",
+  storageBucket: "*****************",
+  messagingSenderId: "************************",
+  appId: "***************************"
 };
 
 // Initialize Firebase
